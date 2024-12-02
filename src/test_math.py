c4ee1113f6f98f2d54ae131491b7418115b7a781
JH2XX <158313400+JH2XX@users.noreply.github.com>
2024-12-01 23:50:39 -0500

    doc: added documentation for integrate linear

--- src/test_math.py
+++ src/test_math.py
@@ -1,9 +1,22 @@
 import numpy as np
+import scipy
+print(scipy.__version__)
 from scipy.integrate import quad
 from scipy.linalg import solve
 
 def integrate_linear(m, b, a, c):
-    
+    """
+    Integrates the linear function f(x) = m*x + b over the range [a, c].
+
+    Parameters:
+        m (float): Slope of the linear function.
+        b (float): Y-intercept of the linear function.
+        a (float): Lower limit of integration.
+        c (float): Upper limit of integration.
+
+    Returns:
+        float: The definite integral value.
+    """ 
     def linear_function(x):
         return m * x + b
 
